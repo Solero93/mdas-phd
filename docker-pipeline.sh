b1de2034453d5ea9ab2158184ba658408199d89d
Christian Soler <solero93@gmail.com>
2018-11-07 19:32:52 +0100

    Initial fixes of the code of session 4

--- docker-pipeline.sh
+++ docker-pipeline.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 docker build -t solero93/votingapp-builder .
 
 docker rm -f myvotingapp-builder || true
@@ -7,7 +8,6 @@ docker run --name myvotingapp-builder -it -v $(pwd):/app solero93/votingapp-buil
 docker build -t solero93/votingapp ./src/votingapp
 
 docker rm -f myvotingapp || true
-docker run --name myvotingapp -d -p 8085:8080 votingapp
+docker run --name myvotingapp -d -p 8085:8080 solero93/votingapp
 
-docker push solero93/votingapp-builder
 docker push solero93/votingapp
