28910e3f0d30a05afb0a954392847daa27e31fbf
Christian Soler <solero93@gmail.com>
2018-11-12 23:38:57 +0100

    Added retry policy to Python code

--- test/votingapp/votingapp.py
+++ test/votingapp/votingapp.py
@@ -2,36 +2,40 @@ import unittest
 import requests
 import os
 
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
 APP_URL = os.environ["APP_URL"]
-
-
-def start_voting(options):
-    return requests.post(APP_URL, json={'topics': options})
-
-
-def put_vote(option):
-    return requests.put(APP_URL, json={'topic': option})
-
-
-def finish_voting():
-    return requests.delete(APP_URL)
 
 
 class TestVotingApp(unittest.TestCase):
     def test_votingapp(self):
-        options = ['Bash', 'Python', 'Go']
+        with requests.Session() as session:
+            retries = Retry(total=5, backoff_factor=1, status_forcelist=[502, 503, 504])
+            session.mount('http://', HTTPAdapter(max_retries=retries))
 
-        start_voting(options)
+            def start_voting(options):
+                return session.post(APP_URL, json={'topics': options})
 
-        for i in range(3):
-            put_vote(options[0])
-        put_vote(options[1])
+            def put_vote(option):
+                return session.put(APP_URL, json={'topic': option})
 
-        expected_winner = options[0]
+            def finish_voting():
+                return session.delete(APP_URL)
 
-        winner = (finish_voting().json())['winner']
+            options = ['Bash', 'Python', 'Go']
 
-        self.assertEqual(winner, expected_winner)
+            start_voting(options)
+
+            for i in range(3):
+                put_vote(options[0])
+            put_vote(options[1])
+
+            expected_winner = options[0]
+
+            winner = (finish_voting().json())['winner']
+
+            self.assertEqual(winner, expected_winner)
 
 
 if __name__ == '__main__':
